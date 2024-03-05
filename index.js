51c188bfe77db48ac08097c42f1ea7920dfcdfc2
Eray Aydın <erayaydinn@protonmail.com>
2024-03-05 18:45:48 +0300

    feat: add typescript support

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import {AppRegistry, Text} from 'react-native';
+import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
+import App from './App';
 
 AppRegistry.registerComponent(
     appName,
-    () => () => <Text>Hello React!</Text>,
+    () => App,
 );
